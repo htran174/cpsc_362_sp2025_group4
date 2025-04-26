0bfb577c065e0450967700ae4ec37c173eb740e7
Kevin <kltran@csu.fullerton.edu>
2025-04-25 23:52:17 -0700

    Added product database

--- website/models.py
+++ website/models.py
@@ -11,3 +11,19 @@ class User(db.Model, UserMixin):
 class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    default_variant_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    category = db.Column(db.String(80), nullable=False)
+    subcategory = db.Column(db.String(80), nullable=False)
+    variant = db.Column(db.String(80), nullable=False)
+    is_default_variant = db.Column(db.Boolean, nullable=False)
+    is_variant_of = db.Relationship('Product', remote_side=[id], lazy=True)
+
+    def validate_variants(self):
+        if self.is_variant_of & self.is_default_variant:
+            raise ValueError('Something is a default variant and a variant of something')
+        return True
