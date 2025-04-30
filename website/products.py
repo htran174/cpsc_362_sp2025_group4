bec7cda326858f000afb77fc889d330cf05e9d28
--global <--global>
2025-04-29 18:05:55 -0600

    Added Necklace Products

--- website/products.py
+++ website/products.py
@@ -62,5 +62,62 @@ products = [
         "price": 250,
         "desc": "Suede Trucker Jacket",
         "image": "images/suede jacket.png"
+    },
+    #Necklaces ID: 51-60
+    {
+        "id": 51,
+        "name": "Aquamarine Heart Necklace",
+        "price": 120,
+        "desc": "Silver Jared Heart-Shaped Jewel glossed in a bright aqua color.",
+        "image": "images/Aquamarine Necklace.png"
+    },
+    {
+        "id": 52,
+        "name": "Ruby Heart Necklace",
+        "price": 220,
+        "desc": "Authentic Ruby Jewel imported into a heart shape.",
+        "image": "images/Ruby Necklace.png"
+    },
+    {
+        "id": 53,
+        "name": "Sapphire Necklace",
+        "price": 450,
+        "desc": "Navy Blue Sapphire Gem Necklace.",
+        "image": "images/Sapphire Necklace.png"
+    },
+    {
+        "id": 54,
+        "name": "Teal Tear Drop Necklace",
+        "price": 600,
+        "desc": "Necklace with an incrusted teal tear-shaped.",
+        "image": "images/Teal Necklace.png"
+    },
+    {
+        "id": 55,
+        "name": "Blue Flower Necklace",
+        "price": 320,
+        "desc": "5-Petal Blue Flower-Shaped Necklace.",
+        "image": "images/Flower Necklace.png"
+    },
+    {
+        "id": 56,
+        "name": "Emerald Heart Necklace",
+        "price": 1200,
+        "desc": "Authentic Emerald Jewel imported and incrusted into a heart shape.",
+        "image": "images/Emerald Necklace.png"
+    },
+    {
+        "id": 57,
+        "name": "Golden Heart Necklace",
+        "price": 500,
+        "desc": "Shiny Gold Yellow Heart-Shaped Necklace.",
+        "image": "images/Golden Necklace.png"
+    },
+    {
+        "id": 58,
+        "name": "Amethyst Necklace",
+        "price": 2200,
+        "desc": "Real Purple Amethyst Incrusted Heart-Shaped Necklace.",
+        "image": "images/Amethyst Necklace.png"
     }
 ]
