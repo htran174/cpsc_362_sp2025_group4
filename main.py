405a332ce84914c9fcb93e504b33fb4c835bec18
kevin-l-tran <kltran@csu.fullerton.edu>
2025-04-30 22:26:24 -0700

    Added dependency installation instructions

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)  
+    app.run(debug=True, port=8000)
+
+# IF YOU ARE MISSING DEPENDENCIES, RUN THIS:
+# pip install -r requirements.txt
