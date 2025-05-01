04583b79f21d89fb4677e75f6bb5a57da3b64ee7
Kevin <kltran@csu.fullerton.edu>
2025-05-01 04:30:48 -0700

    Added functionality to account settings page

--- website/auth.py
+++ website/auth.py
@@ -82,10 +82,59 @@ def logout():
 def profile():
     return render_template('profile/summary.html', user=current_user, current_tab='summary')
 
-@auth.route('/profile/settings')
+@auth.route('/profile/settings', methods=['POST','GET'])
 @login_required
 def profile_settings():
     return render_template('profile/settings.html', user=current_user, current_tab='settings')
+
+@auth.route('/profile/settings/edit', methods=['POST','GET'])
+@login_required
+def profile_settings_edit():
+    success_editing = False
+
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    conf_password = request.form.get('conf_password')
+
+    if email:
+        existing_email = User.query.filter_by(email=email).first()
+        if existing_email:
+            flash('Email address already in use.', category='danger')
+        else:
+            success_editing = True
+            current_user.email = email
+    if username:
+        existing_username = User.query.filter_by(username=username).first()
+        if existing_username:
+            flash('Username already in use.', category='danger')
+        elif len(username) < 4:
+            flash('Username is too short.', category='danger')
+        elif any((not char.isalnum() and char != '_') for char in username):
+            flash('Username contains invalid characters.', category='danger')
+        else:
+            success_editing = True
+            current_user.username = username
+    if password:
+        special_characters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+        if len(password) < 8:
+            flash('Password must be at least 8 characters long.', category='danger')
+        elif not any(char.isupper() for char in password):
+            flash('Password must contain an uppercase character.', category='danger')
+        elif not any(char.isdigit() for char in password):
+            flash('Password must contain a number.', category='danger')
+        elif not any(char in special_characters for char in password):
+            flash('Password must contain a special character.', category='danger')
+        elif password != conf_password:
+            flash('Passwords do not match.', category='danger')
+        else:
+            success_editing = True
+            current_user.password = generate_password_hash(password)
+    db.session.commit()
+    if success_editing:
+        flash('Account edits successful.', category='success')
+
+    return redirect(request.referrer)
 
 @auth.route('/profile/orders')
 @login_required
